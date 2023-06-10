bb93a021a80946f54d291a1b0a981306e3d74650
Ivy Kalliardos <lkalliardos@gmail.com>
2023-06-10 20:41:09 +0200

    Added print_tab_size parameter

--- src/ast.rs
+++ src/ast.rs
@@ -33,26 +33,26 @@ pub enum Expression {
 	BinOp(Box<Expression>, Box<Expression>, BinOp),
 }
 
-fn tabs(mut depth: usize) {
+fn tabs(tab: usize, mut depth: usize) {
 	if depth == 0 { return; }
-	depth *= 4;
+	depth *= tab;
 	print!("{:>depth$}", ' ');
 }
 
 /// TODO: write full ast printer
-pub fn print_expr(expr: &Expression, depth: usize) {
-	tabs(depth);
+pub fn print_expr(expr: &Expression, tab: usize, depth: usize) {
+	tabs(tab, depth);
 	match expr {
 		Expression::Int(i) => println!("int: {i}"),
 		Expression::Float(f) => println!("float: {f}"),
 		Expression::BinOp(lhs, rhs, op) => {
 			println!("binary:");
 				
-			tabs(depth + 1);
+			tabs(tab, depth + 1);
 			println!("operator: {op:?}");
 
-			print_expr(lhs, depth + 1);
-			print_expr(rhs, depth + 1);
+			print_expr(lhs, tab, depth + 1);
+			print_expr(rhs, tab, depth + 1);
 		}
 	};
 }
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,23 @@ use schlange::ast;
 
 #[derive(Parser)]
 struct Args {
-	pub file: String
+	pub file: String,
+	pub print_tab_size: Option<usize>,
 }
 
 fn main() {
 	let args = Args::parse();
 	let src = std::fs::read_to_string(args.file).unwrap();
+	let print_tab_size = match args.print_tab_size {
+		Some(size) => size,
+		None => 2,
+	};
 
 	let mut parser = schlange::parser::Parser::new(&src);
 	let stmts = parser.parse().unwrap();
 
 	stmts.iter().for_each(|stmt| match stmt {
-		ast::Statement::Expression(expr) => ast::print_expr(expr.as_ref(), 0),
+		ast::Statement::Expression(expr) => ast::print_expr(expr.as_ref(), print_tab_size, 0),
 		_ => {}
 	});
 }
